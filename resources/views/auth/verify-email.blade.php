ac55ee1b8eeb1e15a205a12c63fbf9c079b65c33
FaizalDwiA <faizaldwialfarizi@gmail.com>
2026-06-09 21:33:30 +0700

    style: perbaiki tampilan login, register, confirm password, forgot password, reset password, verify email

--- resources/views/auth/verify-email.blade.php
+++ resources/views/auth/verify-email.blade.php
@@ -1,4 +1,12 @@
 <x-guest-layout>
+    @push('styles')
+        <style>
+            body.auth-body {
+                background-image: var(--bg-dark-overlay), url("{{ asset('img/background/verify-email.webp') }}");
+            }
+        </style>
+    @endpush
+
     <div class="mb-4 text-sm text-gray-600">
         {{ __('Thanks for signing up! Before getting started, could you verify your email address by clicking on the link we just emailed to you? If you didn\'t receive the email, we will gladly send you another.') }}
     </div>
@@ -23,7 +31,8 @@
         <form method="POST" action="{{ route('logout') }}">
             @csrf
 
-            <button type="submit" class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+            <button type="submit"
+                class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                 {{ __('Log Out') }}
             </button>
         </form>
