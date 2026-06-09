232d382390d5b246dc90986bb3dbaf3b12a8d144
FaizalDwiA <faizaldwialfarizi@gmail.com>
2026-06-09 17:31:03 +0700

    feat: pasang logo baru edustream

--- resources/views/filament/logo.blade.php
+++ resources/views/filament/logo.blade.php
@@ -1,9 +1,5 @@
 <div class="flex items-center gap-2.5">
-    <div class="flex items-center justify-center w-8 h-8 rounded-lg bg-gradient-to-tr from-orange-600 to-amber-500 shadow-md shadow-orange-500/20">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-5 h-5 text-white">
-            <path d="M4.5 4.5a3 3 0 0 0-3 3v9a3 3 0 0 0 3 3h15a3 3 0 0 0 3-3v-9a3 3 0 0 0-3-3h-15Zm7.65 4.757a.75.75 0 0 1 .85.048l4.25 3.25a.75.75 0 0 1 0 1.19l-4.25 3.25a.75.75 0 0 1-1.15-.6V9.6a.75.75 0 0 1 .3-.593Z" />
-        </svg>
-    </div>
+    <img src="{{ asset('img/logo/logo1.webp') }}" class="w-8 h-8 object-contain rounded-lg shadow-sm" alt="Edustream Logo" />
     <span class="text-xl font-extrabold tracking-tight text-slate-900 dark:text-white">
         Edu<span class="bg-gradient-to-r from-orange-500 to-amber-500 bg-clip-text text-transparent">stream</span>
     </span>
